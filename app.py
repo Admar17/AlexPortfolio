0d2f7bf93a84155b24c19b23e89756983badb2ef
Alex <admar17@byui.edu>
2024-12-04 21:47:50 -0600

    Portfolio Updates

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_bootstrap import Bootstrap
 
 # Initialize Flask app
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with a secure secret key
+app.secret_key = 'your_secure_secret_key'  # Replace with a secure secret key
 CORS(app)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,4 +14,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
-    bootstrap = Bootstrap(app)
